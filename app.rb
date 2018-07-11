30fa873f278b84e574b785b41a12f3fabbe2d112
Vishal Sharma <vsh1819@gmail.com>
2018-07-11 15:48:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
   end
 end
